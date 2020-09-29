45b4445593b44db755050b61b670de08c8f1d904
unknown <samialrahmounn@gmail.com>
2020-09-29 14:28:53 +0200

    initial commit

---
+++ src/main/java/RMIInterface.java
@@ -0,0 +1,10 @@
+
+
+import java.rmi.Remote;
+import java.rmi.RemoteException;
+
+public interface RMIInterface extends Remote {
+
+    public String helloTo(String name) throws RemoteException;
+
+}
