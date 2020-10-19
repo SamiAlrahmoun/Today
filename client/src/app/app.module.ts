00a6ae79225ba51cddea2cf8d57effffb9916d9a
Marvel OYOGHO LAWSON WORA <marvel.oyogho-lawson-wora@etu.univ-nantes.fr>
2020-10-19 10:58:16 +0200

    petite modification de la stucture

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -6,13 +6,17 @@ import { AppComponent } from './app.component';
 import { FeedsComponent } from './feeds/feeds.component';
 import { ProfilComponent } from './profil/profil.component';
 import { CartComponent } from './cart/cart.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { ProductsComponent } from './products/products.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     FeedsComponent,
     ProfilComponent,
-    CartComponent
+    CartComponent,
+    NavbarComponent,
+    ProductsComponent
   ],
   imports: [
     BrowserModule,
