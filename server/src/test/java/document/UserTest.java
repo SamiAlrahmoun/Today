3d851eafcd219c171cf3973a53c983ec2340746d
Sam <samialrahmounn@gmail.com>
2020-10-28 23:45:58 +0100

    adding test

---
+++ server/src/test/java/document/UserTest.java
@@ -0,0 +1,38 @@
+package document;
+
+import org.junit.jupiter.api.Test;
+
+class UserTest {
+
+    @Test
+    void getId() {
+    }
+
+    @Test
+    void setId() {
+    }
+
+    @Test
+    void getUsername() {
+    }
+
+    @Test
+    void setUsername() {
+    }
+
+    @Test
+    void getEmail() {
+    }
+
+    @Test
+    void setEmail() {
+    }
+
+    @Test
+    void getPassword() {
+    }
+
+    @Test
+    void setPassword() {
+    }
+}
